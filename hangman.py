d0c154e201efd2fd38aae76c0a2a9a4a50592515
Connor Jenkins <cjconnorjenkins@gmail.com>
2015-08-19 02:57:41 -0400

    Latest changes

--- hangman.py
+++ hangman.py
@@ -2,7 +2,7 @@ from random import randint
 
 print("Welcome to Hangman! Can you guess all of the letters in the word?")
 
-with open('words/animals') as a:  #  create the pool of words to be randomly chosen from, and split it up into categories
+with open('words/animals') as a:  #Create the pool of words to be randomly chosen from, and split it up into categories.
     animal_list = a.read().splitlines()
 with open ('words/jobs') as b:
     jobs_list = b.read().splitlines()
@@ -14,14 +14,14 @@ with open('words/food_drink') as e:
     food_drink_list = e.read().splitlines()
 
 categories = [animal_list, jobs_list, clothes_list, countries_list, food_drink_list]
-category_names = ["Animals", "Jobs", "Clothes", "Countries", "Food & Drink"]  #  create a list of categories and a corresponding
-                                                                               #  list of category names
+category_names = ["Animals", "Jobs", "Clothes", "Countries", "Food & Drink"]  #Create a list of categories and a corresponding
+                                                                              #list of category names
 
 print("Please select a difficulty:")
 print("Easy (1)  Medium (2)  Hard (3)")
 diff = int(input(""))
 
-assert diff in [1, 2, 3]
+assert diff in [1, 2, 3] #I think there's a better way of doing this but I don't know how.
 print()
 
 difficulties = {1: "Easy", 2: "Medium", 3: "Hard"}
@@ -36,7 +36,7 @@ print("Your category is %s." %cat_name)
 print("You are allowed to guess SIX wrong letters.")
 
 
-def select_word(category, diff):  #  easy is less than 6 letters, medium is 6 letters to 8 letters, hard is 9 letters or more
+def select_word(category, diff):  #Easy is less than 6 letters; medium is 6 letters to 8 letters; hard is 9 letters or more.
     if diff == 1:
         words = [word for word in category if len(word) < 6]
     elif diff == 2:
@@ -51,7 +51,7 @@ print()
 word = select_word(category, diff)
 word = word.upper()
 
-def game(word):
+def game(word): #Should the whole game be one function?
     guesses = 0
     working_word = "-"*len(word)
     guessed_letters = ""
@@ -66,8 +66,8 @@ def game(word):
         if guess in word:
             for i, char in enumerate(word):
                 if char == guess:
-                    working_word = [char for char in working_word] # convert to list to support item assignment
-                    working_word[i] = char                           # in next line
+                    working_word = [char for char in working_word] #Convert to list to support item assignment
+                    working_word[i] = char                         #in next line
                     working_word = ''.join(working_word)
             print("Got one!")
             print(working_word)
@@ -83,12 +83,16 @@ def game(word):
         print(guessed_letters)
         print()
     if working_word == word:
+        print()
         print("You win!")
     else:
         print("You lose!")
         print(word)
 
 game(word)
+
+print()
+input("Press Enter to exit.") #Is this a good way of ending the game?
 
 
 
